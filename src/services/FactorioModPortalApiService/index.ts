ba98554e73715fda1b782edc13fcb5e869b78607
xyzzycgn <120264047+xyzzycgn@users.noreply.github.com>
2026-04-30 19:39:02 +0200

    once again

--- src/services/FactorioModPortalApiService/index.ts
+++ src/services/FactorioModPortalApiService/index.ts
@@ -1,5 +1,4 @@
-import { debug } from '@actions/core';
-import { warning } from "@actions/core";
+import { debug, warning } from '@actions/core';
 import * as fmpe from '@errors/FactorioModPortalApiErrors';
 import { IModInfo } from '@interfaces/IFactorioModInfo';
 import axios, { AxiosError } from 'axios';
@@ -244,7 +243,7 @@ export default class FactorioModPortalApiService {
         if (!e.response.data) throw new fmpe.FactorioModPortalApiError('Unknown error', e.stack);
         const errorResponse = e.response.data as FactorioErrorResponse;
         if (!errorResponse.error) throw new fmpe.FactorioModPortalApiError('Missing error on body', e.stack);
-        if (errorResponse.message) warning("${errorResponse.message}");
+        if (errorResponse.message) warning(errorResponse.message);
         switch (errorResponse.error) {
             case 'InvalidApiKey':
                 throw new fmpe.FactorioModPortalApiInvalidApiTokenError(e.stack);
